77fa483b32c18df114b29d28f357b8c6fa3999ea
aadesope30 <adesope.ajayi@ucalgary.ca>
2026-04-13 02:19:06 -0600

    started backend file

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,33 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_bcrypt import Bcrypt
 
+app = Flask(__name__)
+CORS(app)
+bcrypt = Bcrypt(app)
+
+
+#Registration API
+@app.route('/signup', methods=['POST'])
+
+def registration():
+    data = request.get_json
+    username = data.get('username')
+    email = data.get('email')
+    plain_password = data.get('password')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
